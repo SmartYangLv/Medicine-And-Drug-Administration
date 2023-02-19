30d874c17fc2917a76efa1b46461b17c98cc5775
杨铝 <2239157191@qq.com>
2023-02-19 13:14:44 +0800

    2

--- MedicineAdministration/Form1.Designer.cs
+++ MedicineAdministration/Form1.Designer.cs
@@ -29,33 +29,84 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txb_No = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txb_Password = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(373, 175);
+            this.button1.Location = new System.Drawing.Point(321, 343);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(150, 105);
+            this.button1.Size = new System.Drawing.Size(101, 45);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "登录";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("楷体", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(255, 142);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "账号：";
+            // 
+            // txb_No
+            // 
+            this.txb_No.Location = new System.Drawing.Point(351, 132);
+            this.txb_No.Name = "txb_No";
+            this.txb_No.Size = new System.Drawing.Size(197, 30);
+            this.txb_No.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(255, 234);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "密码：";
+            // 
+            // txb_Password
+            // 
+            this.txb_Password.Location = new System.Drawing.Point(351, 231);
+            this.txb_Password.Name = "txb_Password";
+            this.txb_Password.PasswordChar = '*';
+            this.txb_Password.Size = new System.Drawing.Size(197, 30);
+            this.txb_Password.TabIndex = 4;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(795, 508);
+            this.Controls.Add(this.txb_Password);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txb_No);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("楷体", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txb_No;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txb_Password;
     }
 }
 
--- MedicineAdministration/Form1.cs
+++ MedicineAdministration/Form1.cs
@@ -1,11 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data.SqlClient;
 using System.Windows.Forms;
 
 namespace MedicineAdministration
@@ -15,11 +9,31 @@ namespace MedicineAdministration
         public Form1()
         {
             InitializeComponent();
+            this.StartPosition = FormStartPosition.CenterScreen;
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            
+            SqlConnection sqlConnection = new SqlConnection();
+            sqlConnection.ConnectionString =
+                "Server=(local);Database=MedicineDrug;Integrated Security=sspi";
+            SqlCommand sqlCommand =new SqlCommand();
+            sqlCommand.Connection = sqlConnection;
+            sqlCommand.CommandText = $@"SELECT COUNT(1) FROM tb_User 
+             WHERE NO ='{this.txb_No .Text .Trim ()}' AND Password ='{this .txb_Password .Text .Trim ()}';";
+            sqlConnection.Open();
+            int rowCount=(int)sqlCommand.ExecuteScalar();
+            sqlConnection.Close ();
+            if(rowCount == 1)
+            {
+                MessageBox.Show("登录成功");        
+            }
+            else
+            {
+                MessageBox.Show("用户名或者密码错误，请重新输入");
+                this.txb_Password.Focus();
+                this.txb_Password.SelectAll();
+            }
         }
     }
 }
